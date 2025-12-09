c201b1e86522c1007e116ed4148be72e4140cf59
Ali Ahmed Thawerani <athawerani@gmail.com>
2025-12-09 10:19:42 +0500

    fix

--- java-sample-app/src/Main.java
+++ java-sample-app/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println(greet("Ali"));
+        System.out.println(greet("CustomerA"));
     }
 
     public static String greet(String name) {
-        return "Hello " + name + ", welcome to version 1.0.1!";
+        return "Hello " + name + ", welcome to Customer A version 1.0.1!";
     }
 }
